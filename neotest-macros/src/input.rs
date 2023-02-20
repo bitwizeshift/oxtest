1fbcbc13576aa555fd4539fc49d6f2702fcc147e
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-02-19 19:51:25 -0500

    ✨ Sort input parameters by signature ordering
    The input parameters to `neotest` functions don't actually force
    that the ordering match the ordering of the function parameters.
    However, it's important that the expansion of expressions follow the
    same ordering -- otherwise arguments may not match parameter types.
    
    This adds support for sorting the parameters so that they match the
    signature specified ordering.
    
    This is a partially-completed change, as it does not yet take into
    account generics; which will be handled at a later time. Functionality
    is tentatively added here, but is marked as unused code.

--- neotest-macros/src/input.rs
+++ neotest-macros/src/input.rs
@@ -1,5 +1,8 @@
 use syn::punctuated::Punctuated;
-use syn::Token;
+use syn::token::Comma;
+use syn::{FnArg, Generics, Signature, Token};
+
+use crate::syn_utils::TryIdent;
 
 /// A struct containing parameter inputs that can be specified for a test.
 ///
@@ -214,6 +217,52 @@ pub(crate) struct TestInputs {
   pub parameters: Vec<ParameterInput>,
   pub const_parameters: Vec<ConstParameterInput>,
   pub type_parameters: Vec<TypeParameterInput>,
+}
+
+impl TestInputs {
+  /// Reorders all test inputs to be in the same order as parameters defined in
+  /// the [`Signature`].
+  ///
+  /// # Arguments
+  ///
+  /// * `sig` - the signature to follow the order of.
+  pub fn reorder(&mut self, sig: &Signature) {
+    self.parameters.sort_by(|a, b| {
+      Self::index_of_arg(&sig.inputs, &a.ident).cmp(&Self::index_of_arg(&sig.inputs, &b.ident))
+    })
+  }
+
+  /// Finds the index of the specified ident in the list of function args
+  ///
+  /// # Arguments
+  ///
+  /// * `args` - the list of arguments
+  /// * `ident` - the ident to find
+  fn index_of_arg(args: &Punctuated<FnArg, Comma>, ident: &syn::Ident) -> usize {
+    args
+      .iter()
+      .map(TryIdent::try_ident)
+      .flatten()
+      .position(|v| v.to_string() == ident.to_string())
+      .unwrap()
+  }
+
+  /// Finds the generic index of the specified ident in the list of function args
+  ///
+  /// # Arguments
+  ///
+  /// * `args` - the list of arguments
+  /// * `ident` - the ident to find
+  #[allow(unused)]
+  fn index_of_generic(args: &Generics, ident: &syn::Ident) -> usize {
+    args
+      .params
+      .iter()
+      .map(TryIdent::try_ident)
+      .flatten()
+      .position(|v| v.to_string() == ident.to_string())
+      .unwrap()
+  }
 }
 
 /// An option argument that can be specified as part of the [`neotest`] attribute.
